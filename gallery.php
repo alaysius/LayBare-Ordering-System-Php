9dfc8c606d15ea17569bf498686b83e54df59700
alaysius <82561108+alaysius@users.noreply.github.com>
2021-04-16 15:38:05 +0800

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,20 @@
+<?php include 'headernew.php';?>
+<div class="container">
+
+       <h1 class="title">Gallery</h1>
+       <div class="row gallery">
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_04.jpg" title="Fairview" class="gallery-image" data-gallery><img src="images/photos/hey_04.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_05.jpg" title="Makati" class="gallery-image" data-gallery><img src="images/photos/hey_05.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_09.jpg" title="San Juan" class="gallery-image" data-gallery><img src="images/photos/hey_09.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_10.jpg" title="SM North Edsa" class="gallery-image" data-gallery><img src="images/photos/hey_10.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_11.jpg" title="Robinsons" class="gallery-image" data-gallery><img src="images/photos/hey_11.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_12.jpg" title="Cagayan De Oro" class="gallery-image" data-gallery><img src="images/photos/hey_12.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_13.jpg" title="SM Pasig" class="gallery-image" data-gallery><img src="images/photos/hey_13.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_04.jpg" title="Muntinlupa" class="gallery-image" data-gallery><img src="images/photos/hey_04.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_05.jpg" title="Binondo" class="gallery-image" data-gallery><img src="images/photos/hey_05.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_06.jpg" title="Manila" class="gallery-image" data-gallery><img src="images/photos/hey_06.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_07.jpg" title="Sampaloc" class="gallery-image" data-gallery><img src="images/photos/hey_07.jpg" class="img-responsive"></a></div>
+              <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/hey_09.jpg" title="Cubao,Quezon City" class="gallery-image" data-gallery><img src="images/photos/hey_09.jpg" class="img-responsive"></a></div>
+       </div>
+</div>
+<?php include 'footer.php';?>
